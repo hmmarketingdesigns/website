bc804b857d29771b27c41631b37ec501a82d107c
hmmarketingdesigns <hmmarketingdesigns@gmail.com>
2026-01-12 06:50:37 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import Navigation from './components/Navigation';
 import Footer from './components/Footer';
 import Home from './pages/Home';
+import CaseStudies from './pages/CaseStudies';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/case-studies" element={<CaseStudies />} />
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </main>
