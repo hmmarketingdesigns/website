e1bf01566cf690caab814ae6085670606fc028e1
hmmarketingdesigns <hmmarketingdesigns@gmail.com>
2026-01-12 08:59:51 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,25 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import Navigation from './components/Navigation';
 import Footer from './components/Footer';
 import Home from './pages/Home';
-import CaseStudies from './pages/CaseStudies';
+import CaseStudyPage from './pages/CaseStudyPage';
 
 function App() {
   return (
     <div className="min-h-screen bg-[#0a0a0a]">
       <Navigation />
+
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/case-studies" element={<CaseStudies />} />
+          <Route path="/case-studies" element={<CaseStudyPage />} />
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </main>
+
       <Footer />
     </div>
   );
 }
 
 export default App;
+
